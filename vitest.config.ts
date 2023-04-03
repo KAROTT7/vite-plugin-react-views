0d3ae77bfb8d8d260c0b675f9fe8f3acb27ae09c
karott7 <karott7@gmail.com>
2023-04-03 14:20:46 +0800

    chore: set vitest option  to true

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     setupFiles: ['./__tests__/setup.ts'],
-    include: ['./__tests__/*.spec.ts']
+    include: ['./__tests__/*.spec.ts'],
+    silent: true
   }
 })
